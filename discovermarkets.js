0ad510cdadbd1f02c5af58fbc160c65882735d85
Matt J. Sorenson <matt@sorensonbros.net>
2017-04-28 14:55:37 -0500

    aaahahahaha. I can has local markets data... * found this on my compy from nearly 2 years ago, #hackfargo; * uses a 'put it in a session variable' workaround to getting usda api data rendered out to the page... * no idea if it'll even still work on the current version of meteor *shrug*. * DiscoverMarkets

--- discovermarkets.js
+++ discovermarkets.js
@@ -1,75 +1,64 @@
-Markets = new Mongo.Collection("markets");
+//Markets = new Mongo.Collection("markets");
 
 Meteor.methods({
 
   /** 
    * submit a get request to the restful service zipSearch or locSearch.
-   * @param  {[type]} zip [description]
-   * @return {[type]}     [description]
    */
   getMarkets: function(zipcode) {
     check(zipcode, String);
+
     var marketsApi = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=' + zipcode;
 
-    //this.unblock();
-    return Meteor.http.get( marketsApi );
-    /*
-      function (error, results) {
-        console.log('getMarkets async callback');
-        return results;
-      }*/
-    //);
+    return Meteor.http.get(
+      marketsApi,
+      marketsApiCallback
+    );
   }
 });
 
-
 if (Meteor.isClient) {
+  var _zipcode = '58102';
 
+  // calls 'getMarkets' on the server...
+  // which in turn makes an http.get call to the USDA api...
+  // which in turn invokes a async callback...
+  // which in turn sets a session variable!
+  // TODO: zip code would be user-supplied (search form)
+  Meteor.call('getMarkets', _zipcode);
+  
   Template.body.helpers({
-    marketsNear: [
-      {
-        'id': '1002706',
-        'marketname': 'Downtown Festival Market'  
-      },
-      {
-        'id': '1007642',
-        'marketname': 'Gardener\'s Flea Market',
-      }
-    ]
-
-    /*Meteor.call(
-      'getMarkets',
-      '58102',
-      function(error, result) {
-        //console.log('Meteor.call async callback now');
-        //console.log(result.data);
-        return result.data;
-      }
-    )*/
-  });
-
-  //console.log('about to startup');
-  Meteor.startup(function () {
-    // code to run on client at startup
-    // 
-    /*
-    console.log('about to call usda');
-    
-    var marketsNear = Meteor.call(
-      'getMarkets',
-      '58102',
-      function(error, result) {
-        console.log('Meteor.call async callback now');
-        console.log(result.data);
-        //return result.data;
-      }
-    );
-    */
+    zipcode: _zipcode,
+    markets: function() {
+      // and here we use that session variable :P
+      return Session.get('markets');
+    }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  }); 
+
+// I don't really know the best place for these (plain old) functions yet
+
+
+/**
+ * the async callback used by Meteor.http.get within 'getMarkets'
+ */
+function marketsApiCallback(error, results) {
+  // Session approach/pattern
+  Session.set(
+    'markets', 
+    //results.data.results.forEach(split_distance_marketname)
+    // use .map() instead
+    results.data.results.map(split_distance_marketname)
+  );
+}
+
+/**
+ * USDA search results have a 'marketname' element
+ * that is comprised of distance-in-miles and actual market name, in one string
+ */
+function split_distance_marketname(market) {
+  market.distance = market.marketname.match(/(\d+.\d+)/g)[0]; //  /(\d+)/g
+  market.marketname = market.marketname.substr(market.distance.length + 1);// adjusted
+  return market;
 }
